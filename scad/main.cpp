3f90ee14a0552538cd709fa25e2ad64db69c7878
Giampiero Gabbiani <giampiero@gabbiani.org>
2022-07-24 07:58:07 +0200

    minor changes

--- scad/main.cpp
+++ scad/main.cpp
@@ -1,14 +1,23 @@
-/* Copyright (c) 2012-2017 The ANTLR Project. All rights reserved.
- * Use of this file is governed by the BSD 3-clause license that
- * can be found in the LICENSE.txt file in the project root.
+/*
+ * ADOX scad processor
+ *
+ * Copyright © 2022 Giampiero Gabbiani (giampiero@gabbiani.org)
+ *
+ * This file is part of the 'AutoDox' (ADOX) project.
+ *
+ * ADOX is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ADOX is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ADOX.  If not, see <http: //www.gnu.org/licenses/>.
  */
-
-//
-//  main.cpp
-//  antlr4-cpp-demo
-//
-//  Created by Mike Lischke on 13.03.16.
-//
 
 #include <iostream>
 
