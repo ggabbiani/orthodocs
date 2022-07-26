110bfcc4db309a0eb93a40f0e8371cf39088db6a
Giampiero Gabbiani <giampiero@gabbiani.org>
2022-07-26 04:58:17 +0200

    various updates in main.cpp leading to bost addition into the application build

--- scad/main.cpp
+++ scad/main.cpp
@@ -22,22 +22,26 @@
 #include "antlr4-runtime.h"
 #include "SCADLexer.h"
 #include "SCADParser.h"
+#include "SCADParserBaseListener.h"
+
+#include <tree/ParseTreeWalker.h>
 
 #include "CLI11.hpp"
-
+#include <boost/filesystem.hpp>
 #include <iostream>
 #include <sstream>
 
 using namespace std;
 using namespace scad;
 using namespace antlr4;
+namespace fs=boost::filesystem;
 
-class ErrorListener : public BaseErrorListener {
+class ErrorHandler : public BaseErrorListener {
   void syntaxError(Recognizer *recognizer, Token * offendingSymbol, size_t line, size_t charPositionInLine,
-    const std::string &msg, std::exception_ptr e) override;
+    const string &msg, exception_ptr e) override;
 };
 
-void ErrorListener::syntaxError(Recognizer *recognizer, Token * offendingSymbol, size_t line, size_t charPositionInLine, const std::string &msg, std::exception_ptr e) {
+void ErrorHandler::syntaxError(Recognizer *recognizer, Token * offendingSymbol, size_t line, size_t charPositionInLine, const std::string &msg, std::exception_ptr e) {
   ostringstream s;
   // cout  << "Offending symbol  : " << offendingSymbol->toString() << endl
   //       << "Grammar file name : " << recognizer->getGrammarFileName() << endl;
@@ -45,43 +49,69 @@ void ErrorListener::syntaxError(Recognizer *recognizer, Token * offendingSymbol,
   throw std::invalid_argument(s.str());
 }
 
-int main(int argc, const char *argv[]) {
-  string file;
-  bool show_tokens = false;
-  CLI::App app{"ADOX stands for Auto Docs: it is meant for automatic documentation building from SCAD programs."};
-  app.add_option("file", file, "the OpenSCAD file to be checked")
-    ->required()
-    ->check(CLI::ExistingFile);
-  app.add_flag("--tokens,-t",show_tokens,"when true tokens are listed");
-  try {
-    app.parse(argc, argv);
-  } catch (const CLI::ParseError &e) {
-    return app.exit(e);
+class DocGenerator : public SCADParserBaseListener {
+public:
+  DocGenerator(const fs::path &source) : _package(source.filename().stem().string()) {}
+
+  void enterPkg(SCADParser::PkgContext * ctx) override {
+    cout << "Package " << _package << endl;
   }
 
-  auto              result = EXIT_SUCCESS;
-  ifstream          stream(file);
-  ANTLRInputStream  input(stream);
-  SCADLexer         lexer(&input);
-  CommonTokenStream tokens(&lexer);
+  void enterNamed_module_definition(SCADParser::Named_module_definitionContext * ctx) override {
+    auto identifier   = ctx->ID()->getText();
+    auto description  = "Not implemented yet.";
+    _index[identifier]  = description;
+    cout << "Module " << identifier << " found." << endl;
+  }
+private:
+  // a little semantic...
+  using ID = std::string;
+  using Description = std::string;
+  string _package;
+  map<ID,Description> _index;
+};
 
-  tokens.fill();
+int main(int argc, const char *argv[]) {
+  CLI::App app{"ADOX stands for Auto Docs, automatic documentation generation for the OpenSCAD language."};
+  auto      result = EXIT_SUCCESS;
+  fs::path  file;
+  bool      show_tokens = false;
 
-  if (show_tokens)
-    for (auto token : tokens.getTokens())
-      cout << token->toString() << endl;
-
-  SCADParser    parser(&tokens);
-  parser.removeErrorListeners();
-  ErrorListener listener;
-  parser.addErrorListener(&listener);
+  app.add_option("file", file, "SCAD source file")
+    ->required()
+    ->check(CLI::ExistingFile);
+  app.add_flag("--tokens,-t",show_tokens,"Manages token listing");
 
   try {
-    tree::ParseTree*  tree = parser.expr();
-    cout << tree->toStringTree(&parser) << endl << endl;
+    app.parse(argc, argv);
+    ifstream          stream(file);
+    ANTLRInputStream  input(stream);
+    SCADLexer         lexer(&input);
+    CommonTokenStream tokens(&lexer);
+
+    if (show_tokens) {
+      tokens.fill();
+      for (auto token : tokens.getTokens())
+        cout << token->toString() << endl;
+    }
+
+    SCADParser    parser(&tokens);
+
+    // error management
+    parser.removeErrorListeners();
+    ErrorHandler listener;
+    parser.addErrorListener(&listener);
+
+    DocGenerator  generator(file.c_str());
+    // parse tree depth-first traverse
+    tree::ParseTreeWalker  walker;
+    tree::ParseTree       *tree = parser.pkg();
+    walker.walk(&generator,tree);
+  } catch (const CLI::ParseError &e) {
+    result  = app.exit(e);
   } catch(const exception &error) {
     cerr << error.what() << endl;
-    result = EXIT_FAILURE;
+    result  = EXIT_FAILURE;
   }
   return result;
 }
