3d7d23db52e5418b5342b0261d9e8bde45601119
Giampiero Gabbiani <giampiero@gabbiani.org>
2022-08-10 11:40:06 +0200

    updated

--- scad/include/generator.h
+++ scad/include/generator.h
@@ -43,16 +43,19 @@ struct Generator : public scad::SCADBaseListener {
 
   Generator(const boost::filesystem::path &source);
   
-  void enterFunction_def(scad::SCADParser::Function_defContext *ctx)  override;
-  void exitFunction_def(scad::SCADParser::Function_defContext *ctx)   override;
-  void enterModule_def(scad::SCADParser::Module_defContext * ctx)     override;
-  void exitModule_def(scad::SCADParser::Module_defContext * ctx)      override;
   void enterAnnotation(scad::SCADParser::AnnotationContext *ctx)      override;
-  void enterParameter(scad::SCADParser::ParameterContext *ctx)        override;
-  void exitParameter(scad::SCADParser::ParameterContext *ctx)         override;
-  void enterLookup(scad::SCADParser::LookupContext *ctx)              override;
   void enterAssignment(scad::SCADParser::AssignmentContext *ctx)      override;
+  void enterFunction_def(scad::SCADParser::Function_defContext *ctx)  override;
+  void enterLookup(scad::SCADParser::LookupContext *ctx)              override;
+  void enterModule_def(scad::SCADParser::Module_defContext * ctx)     override;
+  void enterParameter(scad::SCADParser::ParameterContext *ctx)        override;
+  void enterPkg(scad::SCADParser::PkgContext *ctx)                    override;
+
   void exitAssignment(scad::SCADParser::AssignmentContext *ctx)       override;
+  void exitFunction_def(scad::SCADParser::Function_defContext *ctx)   override;
+  void exitModule_def(scad::SCADParser::Module_defContext * ctx)      override;
+  void exitParameter(scad::SCADParser::ParameterContext *ctx)         override;
+  void exitPkg(scad::SCADParser::PkgContext *ctx)                     override;
 };
 
 }
