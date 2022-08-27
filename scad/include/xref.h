e2b5e59e8089e6e5e49eb6be95764dae729fe204
Giampiero Gabbiani <giampiero@gabbiani.org>
2022-08-27 17:33:16 +0200

    Implemented case insensible ToC

--- scad/include/xref.h
+++ scad/include/xref.h
@@ -3,21 +3,31 @@
 #include "document.h"
 #include "writers.h"
 
+#include <algorithm>
 #include <iostream>
 #include <map>
 #include <string>
 
 class Index {
+  friend doc::writer::Mdown;
 public:
+  struct NoCase {
+    bool operator() (const std::string& s1, const std::string& s2) const {
+      std::string str1(s1.length(),' ');
+      std::string str2(s2.length(),' ');
+      std::transform(s1.begin(), s1.end(), str1.begin(), ::tolower);
+      std::transform(s2.begin(), s2.end(), str2.begin(), ::tolower);
+      return  str1 < str2;
+    }
+  };
+
   using KeyType     = doc::Name;
   using MappedType  = doc::ItemPtr;
-  using Map         = std::map<KeyType,MappedType>;
+  using Map         = std::map<KeyType,MappedType,NoCase>;
   
   Index() {}
-  // move items into index, contextually enriching item with source uri
+  // move Document items into index, contextually enriching with source uri
   void add(Document &items);
-  void serialize(const doc::Writer &writer) const;
-  void deserialize(std::istream &is) {}
 private:
   void insert(const doc::Name &index,MappedType &item) {_map.emplace(index,std::move(item));}
   Map _map;
--- scad/src/xref.cpp
+++ scad/src/xref.cpp
@@ -5,40 +5,13 @@
 
 using namespace std;
 
-// FIXME: should be in 'document' namespace!
-static string tstring(const doc::Item &item) {
-  return string(
-      is<const doc::Function>(item) ? "function" 
-    : is<const doc::Module>(item)   ? "module" 
-    : is<const doc::Package>(item)  ? "package" 
-    : is<const doc::Variable>(item) ? "variable" 
-    : "UNKNOWN"
-  );
-}
-
 void Index::add(Document &document) {
   // ItemMap format:
   // Key    ==> "function|module|variable <item name>"
   // Value  ==> unique_ptr<doc::Item>
   for(auto &item: document) {
     // toc Key ==> "<item name> (function|module|variable)"
-    auto key  = item.second->name+" ("+tstring(*item.second)+')';
+    auto key  = item.second->name+" ("+item.second->type()+')';
     insert(key,item.second);
   }
-}
-
-void Index::serialize(const doc::Writer &writer) const {
-  writer.out() << writer.H("Table of Contents",1) << endl;
-  // ToC format:
-  // Key    ==> "<item name> (function|module|variable)"
-  // Value  ==> unique_ptr<doc::Item>
-  for(auto &item: _map) {
-    // see https://www.markdownguide.org/extended-syntax/#heading-ids
-    auto id = tstring(*item.second)+'-'+item.second->name;
-    // replace(id.begin(), id.end(), ' ', '-');
-    auto link = item.second->uri.string();
-    auto title = item.first;
-    writer.out() << "- [" << title << "](" << link << "#" << id << ")" << endl;
-  }
-
 }
