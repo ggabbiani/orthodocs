f07f2d79476041157d5dd9a533d82e296eb99e23
Giampiero Gabbiani <giampiero@gabbiani.org>
2022-10-08 09:16:14 +0200

    Coding smell fixes

--- extensions/SCAD/include/scad/xref.h
+++ extensions/SCAD/include/scad/xref.h
@@ -6,7 +6,6 @@ namespace scad {
 
 class XRef : public orthodocs::AbstractXRef {
 public:
-  ~XRef() = default;
   /**
    * the xref key of an item is calculated from its name and the name of the package in which it's defined
    */
