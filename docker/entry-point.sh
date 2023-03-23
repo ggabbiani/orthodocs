999049383f6cd1135a481d0702414c4715a6199f
Giampiero Gabbiani <giampiero@gabbiani.org>
2023-03-23 11:51:26 +0100

    updated

--- docker/entry-point.sh
+++ docker/entry-point.sh
@@ -3,6 +3,6 @@
 git config --global --add safe.directory '*'
 cmake -GNinja -S /in -B /out -DCMAKE_BUILD_TYPE=Release -DODOX_TESTS=ON
 cmake --build /out --config Release
+cd /out
 ctest
 cpack
-cp *.rpm /out
